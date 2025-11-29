31a9b2c3dc05081e5223e3e58ab88ec0e70ebbb1
raghav-webseeder <mishra.raghav@webseeder.tech>
2025-11-29 17:57:36 +0530

    commented rate limiter

--- src/server.js
+++ src/server.js
@@ -55,12 +55,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Rate limit - global
-const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
-  message: "Too many requests from this IP, please try again later.",
-});
-app.use(limiter);
+// const limiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 100,
+//   message: "Too many requests from this IP, please try again later.",
+// });
+// app.use(limiter);
 
 // Rate limit - login/signup special
 app.use(
